72036ac40b944dce16a0ac45a5d3cc69ebb45e05
manojnaraharisetti <manojnaraharisetti2004@gmail.com>
2024-06-18 16:06:12 +0530

    ng2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let playerSymbol = ""; // 'X' or 'O'
 let currentTurn = 'X'; // Track whose turn it is
 let count = 0; // to track the draw game
 
-const socket = new WebSocket('wss://tic-tac-toe-gwyu.onrender.com');
+const socket = new WebSocket('ws://localhost:3000');
 let player = 0;
 
 socket.addEventListener('message', event => {
